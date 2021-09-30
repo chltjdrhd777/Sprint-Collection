c5849b3f44e773ac08862e72fa2c09dd769e248a
heewonkim-dev <85002287+heewonkim-dev@users.noreply.github.com>
2021-09-30 16:40:51 +0900

    Add files via upload

---
+++ Section-01-Single-Page-Application/im-ha-section-1-master/src/__test__/index.test.js
@@ -0,0 +1,11 @@
+describe('Thumbnail 컴포넌트 테스트', () => {
+  require('./Thumbnail.test')
+});
+
+describe('App 컴포넌트 테스트', () => {
+  require('./App.test')
+});
+
+describe('Gallery 컴포넌트 테스트', () => {
+  require('./Gallery.test')
+});
